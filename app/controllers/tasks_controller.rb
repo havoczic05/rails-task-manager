a78c44ca0d72e72a1b7724150d113faf6ac20f20
hugo salazar <hugog.salazarl@gmail.com>
2024-08-27 23:02:15 -0500

    Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_restaurants, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -14,21 +14,19 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+
+    redirect_to tasks_path
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path, status: :see_other
@@ -38,5 +36,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_restaurants
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'tasks', to: 'tasks#index' # read (all)
-  get 'tasks/new', to: 'tasks#new', as: :new_task # new
-  get 'tasks/:id', to: 'tasks#show', as: :task # read (one) o find
-  post 'tasks', to: 'tasks#create' # new - para enviar la información
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit # edit
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+  # get 'tasks', to: 'tasks#index' # read (all)
+  # get 'tasks/new', to: 'tasks#new', as: :new_task # new
+  # get 'tasks/:id', to: 'tasks#show', as: :task # read (one) o find
+  # post 'tasks', to: 'tasks#create' # new - para enviar la información
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit # edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
